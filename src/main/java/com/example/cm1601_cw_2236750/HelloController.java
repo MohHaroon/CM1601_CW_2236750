a53545b97352cc1d20fa691a462d65b742844b15
Mohamed Haroon <116776845+MohHaroon@users.noreply.github.com>
2023-03-17 22:45:27 +0430

    add scene added

--- src/main/java/com/example/cm1601_cw_2236750/HelloController.java
+++ src/main/java/com/example/cm1601_cw_2236750/HelloController.java
@@ -1,14 +1,35 @@
 package com.example.cm1601_cw_2236750;
 
+import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.io.IOException;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+public class HelloController{
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void mainScene(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("racing.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void addDriver(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("addDriver.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
