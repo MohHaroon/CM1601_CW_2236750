844de0c7ba3049993be324bfb1a0724f83d9e800
Mohamed Haroon <116776845+MohHaroon@users.noreply.github.com>
2023-03-17 16:49:37 +0430

    Racing beginning build
    Racing beginning build

---
+++ src/main/java/com/example/cm1601_cw_2236750/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.cm1601_cw_2236750;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
